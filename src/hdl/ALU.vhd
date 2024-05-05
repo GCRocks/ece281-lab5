05fdc2b1a9792ac26740ca4a93fbc8d2ca29902c
GCRocks <141965820+GCRocks@users.noreply.github.com>
2024-05-04 23:43:44 -0600

    some of part a and b work

--- src/hdl/ALU.vhd
+++ src/hdl/ALU.vhd
@@ -35,17 +35,43 @@ library ieee;
 
 
 entity ALU is
--- TODO
+    port(
+        i_A : in std_logic_vector (7 downto 0);
+        i_B : in std_logic_vector (7 downto 0);
+        i_op : in std_logic_vector (2 downto 0);
+        o_result : out std_logic_vector (7 downto 0);
+        o_flags : out std_logic_vector (2 downto 0)
+    );
 end ALU;
 
 architecture behavioral of ALU is 
   
 	-- declare components and signals
-
+component halfAdder is 
+	port(
+	    i_A     : in  std_logic; -- 1-bit input port
+        i_B     : in  std_logic;
+        o_Cout  : out std_logic;  
+        o_S     : out std_logic  -- 1-bit output port
+	);
+end component halfAdder;
+    
+component TDM4 is
+    port(
+    
+    );
+end component TDM4;
   
 begin
 	-- PORT MAPS ----------------------------------------
-
+    
+    o_result <= i_A xor i_B when (i_op = x"0"); --add when i_op is 0
+    o_result <= i_A xor not i_B when (i_op = x"1"); --subtract when i_op is 1
+    o_result <= i_A and i_B when (i_op = x"2"); --AND when i_op is 2
+    o_result <= i_A or i_B when (i_op = x"3"); --OR when i_op is 3
+    --o_result <= i_A<<i_B when (i_op = x"4"); --left shift when i_op is 4
+    --o_result <= i_A>>i_B when (i_op = x"5"); -- right shift when i_op is 5
+    
 	
 	
 	-- CONCURRENT STATEMENTS ----------------------------
