5efb87015eed0b0aa14c2bc4ddfa7105d4880abf
GCRocks <141965820+GCRocks@users.noreply.github.com>
2024-05-10 10:37:23 -0600

    fixing ALU tb

--- src/hdl/ALU_tb.vhd
+++ src/hdl/ALU_tb.vhd
@@ -67,19 +67,19 @@ ALU_inst : ALU
     
     w_A <= x"1"; w_B <= x"1"; sw <= x"0"; wait for 10 ns;
         assert (w_result = x"2" and LEDs = x"0") report "ALU doesn't add properly" severity failure;
-    w_A <= x"0"; w_B <= x"1"; sw <= x"1"; wait for 10 ns;
+    w_A <= "00000000"; w_B <= "00000001"; sw <= "001"; wait for 10 ns;
         assert (w_result(7) = '1' and LEDs = x"1") report "flag doesn't show negative result" severity failure;
-    w_A <= x"1"; w_B <= x"1"; sw <= x"1"; wait for 10 ns;
+    w_A <= "00000001"; w_B <= x"00000001"; sw <= "001"; wait for 10 ns;
         assert (w_result = x"0" and LEDs = x"0") report "flag doesn't show negative result" severity failure;
-    w_A(7) <= '1'; w_B(7) <= '1'; sw <= x"0";  wait for 10 ns;
+    w_A <= ('1' & (6 downto 0 => '0')); w_B <= ('1' & (6 downto 0 => '0')); sw <= x"0";  wait for 10 ns;
         assert (w_result = x"0" and LEDs = x"3") report "flag doesn't show carry out" severity failure; 
-    w_A <= x"1"; w_B <= x"0"; sw <= x"2"; wait for 10 ns;
+    w_A <= "00000001"; w_B <= "00000000"; sw <= "010"; wait for 10 ns;
         assert (w_result = x"0" and LEDs = x"0") report "error with and operation" severity failure;
-    w_A <= x"1"; w_B <= x"0"; sw <= x"3"; wait for 10 ns;
+    w_A <= "00000001"; w_B <= "00000000"; sw <= "011"; wait for 10 ns;
         assert (w_result = x"1" and LEDs = x"0") report "error with or operation" severity failure; 
-    w_A <= x"1"; w_B <= x"1"; sw <= x"4"; wait for 10 ns;
+    w_A <= "00000001"; w_B <= "00000001"; sw <= "100"; wait for 10 ns;
         assert (w_result <= x"2" and LEDs = x"0") report "error with left logical shift" severity failure;
-    w_A <= x"2"; w_B <= x"1"; sw <= x"5"; wait for 10 ns;
+    w_A <= "00000010"; w_B <= "00000001"; sw <= "101"; wait for 10 ns;
         assert (w_result <= x"1" and LEDs <= x"0") report "error with right logical shift" severity failure; 
            
             wait;
